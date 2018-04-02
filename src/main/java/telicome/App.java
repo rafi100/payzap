5082fcf4efefd9c0afb1a7fc92fd9d8fe68a7eb6
rafi3 <rafi3linux@gmail.com>
2018-04-02 09:56:58 +0530

    msg

---
+++ src/main/java/telicome/App.java
@@ -0,0 +1,16 @@
+package network;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello world!" );
+
+
+        System.out.println( "Hello world!" );
+
+        System.out.println( "Hello java world!" );
+		
+    }
+}
